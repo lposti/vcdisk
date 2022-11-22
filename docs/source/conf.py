34551aec48b662e48c4b904fc57886f9336543e2
Lorenzo Posti <lorenzo.posti@gmail.com>
2022-11-22 19:54:08 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,9 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# The master toctree document.
+master_doc = 'index'
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
