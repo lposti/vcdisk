640da648572e59f4d0a9d91adbdf58fc7dd517d1
Lorenzo Posti <lorenzo.posti@gmail.com>
2022-11-22 19:56:48 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,8 @@
 # Configuration file for the Sphinx documentation builder.
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information
 
@@ -31,9 +31,6 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-
-# The master toctree document.
-master_doc = 'index'
 
 # -- Options for HTML output
 
